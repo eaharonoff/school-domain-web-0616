a1deb9fe46f1bc9f7ce9bf7ef1b7162006275a32
eaharonoff <emanuel.aharonoff@gmail.com>
2016-06-10 09:04:32 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
 # code here!
+class School
+    
+  def initialize (school)
+    @school_hash = {}
+  end
+
+  def roster
+    @school_hash
+  end
+
+  def add_student(student_name, grade)
+    @school_hash[grade] ||= []
+    @school_hash[grade] << student_name
+  end
+
+  def grade (grade)
+    @school_hash[grade]
+  end
+
+  def sort
+    @school_hash.each_key{|k| @school_hash[k].sort!}
+  end
+
+end
